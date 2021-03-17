56d3d287ef65d1e9b7421cf7520ba2fd3eca9d4d
Jyoti Mishra <mishranishi02@gmail.com>
2021-03-18 00:50:53 +0530

    Improving the Proof of Work System

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+
+const hexToBinary = require('hex-to-binary');
 const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
@@ -28,7 +30,7 @@ class Block
             timestamp = Date.now();
             difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp});
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        } while(hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while(hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
     }
@@ -39,7 +41,7 @@ class Block
         if(difficulty < 1) return 1;
 
         if(timestamp - originalBlock.timestamp > MINE_RATE ) return difficulty -1;
-
+ 
         return difficulty + 1;
     }
 }
